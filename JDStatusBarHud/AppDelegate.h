5c2d5e456c9d728e0366d99d5606897364841020
Jude309307972 <jude@judedemac-mini.local>
2016-09-05 18:45:47 +0800

    Initial Commit

---
+++ JDStatusBarHud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JDStatusBarHud
+//
+//  Created by Jude on 16/9/5.
+//  Copyright © 2016年 Jude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JDStatusBarHud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JDStatusBarHud
+//
+//  Created by Jude on 16/9/5.
+//  Copyright © 2016年 Jude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
